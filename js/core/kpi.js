da55f7bc9e00399f859333a4a06b11ec6b5b2cfd
marcosgarcon <socramgpmo@hotmail.com>
2026-02-19 03:55:27 -0300

    Delete js directory

--- js/core/kpi.js
+++
@@ -1,5 +0,0 @@
-function calculateKPIDeviation(target, actual) {
-
-    let deviation = Math.abs(target - actual) / target;
-    return (deviation * 100).toFixed(2);
-}
--- js/core/risk.js
+++
@@ -1,7 +0,0 @@
-function calculateRisk(cpk, trend) {
-
-    let cepScore = Math.max(0, (1.67 - cpk) * 40);
-    let trendScore = Math.abs(trend) * 5;
-
-    return (cepScore + trendScore).toFixed(1);
-}
